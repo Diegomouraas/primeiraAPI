db1ebbb313293552f2ee14800349603f87f7c998
Diego Moura <diegomoura.contact@gmail.com>
2022-03-22 11:08:01 -0300

    Dados Validados e Arquivados no DB.

--- routes/in.js
+++ routes/in.js
@@ -15,77 +15,114 @@ const Usuarios = mongoose.model('usuarios');
         console.log(req.body.cpf[1])
         var erros = []; // array que armazena os erros
 
-        if(req.body == undefined) {
-            res.send("Escreveu errado mermao")
-        }else if(!req.body.cpf || req.body.cpf == null || req.body.cpf == undefined || 
-            req.body.cpf.lenght > 11 || req.body.cpf.lenght == 0) {
+        // Verificando se o formulario esta vazio
+            if(req.body == undefined) {
+                erros.push("Dados Invalidos")
+            }else {
+                
+                // Verificando dados do Formulario
+                    // Nome
+                    if(!req.body.nome || req.body.nome == null || req.body.nome == undefined ||req.body.nome.length > 100){
+                        erros.push("Nome Invalido")
+                        console.log("Nome Invalido")
+                    }
 
-            erros.push("CPF invalido")
-            res.redirect('/in')
-        }else{
-            let fistDigCpf = ((
-            (req.body.cpf[0] * 1) +
-            (req.body.cpf[1] * 2) +
-            (req.body.cpf[2] * 3) +
-            (req.body.cpf[3] * 4) +
-            (req.body.cpf[4] * 5) +
-            (req.body.cpf[5] * 6) +
-            (req.body.cpf[6] * 7) +
-            (req.body.cpf[7] * 8) +
-            (req.body.cpf[8] * 9) ) % 11)
+                    // CPF
+                    if(!req.body.cpf || req.body.cpf == null || req.body.cpf == undefined || 
+                        req.body.cpf.lenght > 11 || req.body.cpf.lenght == 0) {
 
-            let segDigCpf = ((
-                (req.body.cpf[0] * 0) +
-                (req.body.cpf[1] * 1) +
-                (req.body.cpf[2] * 2) +
-                (req.body.cpf[3] * 3) +
-                (req.body.cpf[4] * 4) +
-                (req.body.cpf[5] * 5) +
-                (req.body.cpf[6] * 6) +
-                (req.body.cpf[7] * 7) +
-                (req.body.cpf[8] * 8) +
-                (req.body.cpf[9] * 9) ) % 11)
+                        erros.push("CPF invalido")
+                        
+                    }else{
+                        // validando CPF
+                        let fistDigCpf = ((
+                            (req.body.cpf[0] * 1) +
+                            (req.body.cpf[1] * 2) +
+                            (req.body.cpf[2] * 3) +
+                            (req.body.cpf[3] * 4) +
+                            (req.body.cpf[4] * 5) +
+                            (req.body.cpf[5] * 6) +
+                            (req.body.cpf[6] * 7) +
+                            (req.body.cpf[7] * 8) +
+                            (req.body.cpf[8] * 9) ) % 11)
 
-            let digitoCalc = ((fistDigCpf * 10) + segDigCpf)
-            let digitoReal = (parseInt(req.body.cpf[9] * 10) + parseInt(req.body.cpf[10]))
+                        let segDigCpf = ((
+                            (req.body.cpf[0] * 0) +
+                            (req.body.cpf[1] * 1) +
+                            (req.body.cpf[2] * 2) +
+                            (req.body.cpf[3] * 3) +
+                            (req.body.cpf[4] * 4) +
+                            (req.body.cpf[5] * 5) +
+                            (req.body.cpf[6] * 6) +
+                            (req.body.cpf[7] * 7) +
+                            (req.body.cpf[8] * 8) +
+                            (req.body.cpf[9] * 9) ) % 11)
 
-            if(digitoCalc == digitoReal){
-                console.log("CPF Verdadeiro")
-                res.send("CPF Verdadeiro")
-            }else{
-                console.log("CPF Invalido ")
-                console.log(digitoCalc)
-                console.log(digitoReal)
+                        if(fistDigCpf == 10) {
+                            fistDigCpf = 0
+                        }else if(segDigCpf == 10) {
+                            segDigCpf = 0
+                        }
 
-                res.redirect('/in')
+                        let digitoCalc = ((fistDigCpf * 10) + segDigCpf)
+                        let digitoReal = (parseInt(req.body.cpf[9] * 10) + parseInt(req.body.cpf[10]))
+
+                        if(digitoCalc == digitoReal){
+                            console.log("CPF Verdadeiro")
+                           
+                        }else{
+                            console.log("CPF Invalido ")
+                            console.log(digitoCalc)
+                            console.log(digitoReal)
+                            erros.push("CPF Invalido")
+                            
+                        }
+                    }
+
+                    // Data de nascimento 
+                    if(!req.body.dataNascimento || req.body.dataNascimento == null || req.body.dataNascimento == undefined){
+                        erros.push("Data de Nascimento invalida")
+                        
+                    } else {
+                        // Validando data de nascimento
+
+                        let dia = parseInt(req.body.dataNascimento[0] * 10) + parseInt(req.body.dataNascimento[1])
+
+                        let mes = parseInt(req.body.dataNascimento[2] * 10) + parseInt(req.body.dataNascimento[3])
+
+                        let ano = parseInt(req.body.dataNascimento[4] * 1000) + parseInt(req.body.dataNascimento[5] * 100) 
+                                + parseInt(req.body.dataNascimento[6] * 10) + parseInt(req.body.dataNascimento[7])
+
+                        const date = new Date();
+
+                        if(dia < 1 || dia > 31 || mes < 1 || mes > 12 || ano < 1900 || ano > date.getFullYear()){
+                            erros.push("Data de Nascimento invalida")
+                            console.log("Data de Nascimento invalida")
+                        } else {
+                            console.log("data de Nascimento: ", dia, "/", mes, "/", ano)
+                        }
+                    }
+
+                }
+
+        // Arquivo
+            if(erros.length > 0) {
+                res.send("Dados Invalidos")
+            } else {
+                const dadosDoUsuario = {
+                    nome: req.body.nome,
+                    cpf: req.body.cpf,
+                    dataNascimento: req.body.dataNascimento
+                }
+
+                new Usuarios(dadosDoUsuario).save().then(() => {
+                    res.send("Usuario cadastrado!")
+                }).catch((err) => {
+                    res.send("Erro ao Cadastrar Usuarios!")
+                    console.log("Erro no cadastro: " + err)
+                })
             }
-
             
-        }
-
-
-        // validação dos parametros
-            /*if(!req.body.nome || req.body.nome == null || req.body.nome == undefined) {
-                erros.push("Digite um nome valido")
-            }
-
-            if(!req.body.cpf || req.body.cpf == null || req.body.cpf == undefined || 
-                req.body.cpf.lenght > 11 || req.body.cpf.lenght == 0) {
-
-                erros.push("CPF invalido")
-            }else{
-                let fistDigCpf = (2314)
-            }
-            
-            let rg_calc = (req.body.cpf)
-
-        
-
-            
-
-            if(!req.body.cpf || req.body.cpf == null || req.body.cpf == undefined) {
-                erros.push("CPF invalido")
-            }*/
     })
 
 
